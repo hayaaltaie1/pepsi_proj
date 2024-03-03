acaaf2e5aee5e2bf3335ff1781631edfa5b9687c
unknown <mernstack09@gmail.com>
2024-03-03 20:15:02 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+let mainImg = document.querySelector('.main-img');
+let main = document.querySelector('.main')
+
+function chgimg(img){
+    mainImg.src = img
+}
+
+function colors(color) {
+    main.style.background= color;
+    
+}
